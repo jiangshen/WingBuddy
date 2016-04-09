6d1d896996afcf65f170bca2fd4a9ae4ae11a796
Deb Banerji <debkbanerji@gmail.com>
2016-04-09 10:54:23 -0400

    User
    Created rough outline of user class

---
+++ app/src/main/java/codemonkey/wingbuddy/User.java
@@ -0,0 +1,45 @@
+package codemonkey.wingbuddy;
+
+/**
+ * Created by Deb Banerji on 09-Apr-16.
+ */
+public class User {
+    private String userName;
+    private String phoneNumber;
+
+    private int wingPoints;
+
+    public User(String userName, String phoneNumber) {
+        this.userName = userName;
+        this.phoneNumber = phoneNumber;
+        wingPoints = 0;
+    }
+
+    public String getName() {
+        return userName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public int getWingPoints() {
+        return wingPoints;
+    }
+
+    public int increaseWingPoints() {
+        wingPoints++;
+    }
+
+    public int increaseWingPoints(int num) {
+        wingPoints+=num;
+    }
+
+    public int decreaseWingPoints() {
+        wingPoints-;
+    }
+
+    public int decreaseWingPoints(int num) {
+        wingPoints-=num;
+    }
+}
