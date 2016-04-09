d8d0ac74e7f6f9e74b15cb45e9d608a261190f1e
jiangshen <jiangshen16@icloud.com>
2016-04-09 17:02:19 -0400

    Added Room Status and Last movement

--- app/src/main/java/codemonkey/wingbuddy/MainActivity.java
+++ app/src/main/java/codemonkey/wingbuddy/MainActivity.java
@@ -3,6 +3,7 @@ package codemonkey.wingbuddy;
 import android.annotation.TargetApi;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -21,12 +22,17 @@ import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 import com.firebase.client.ValueEventListener;
 
+import org.w3c.dom.Text;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
     Button refreshButton;
+    TextView tvRoomStatus;
+    TextView tvLastSeen;
+    TextView tvTimeElapsed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,15 +46,16 @@ public class MainActivity extends AppCompatActivity {
 
         final long longDateValue[] = new long[1];
 
-
         Firebase firebase = new Firebase("https://wingbuddy.firebaseio.com/");
-        final TextView timeStampView = (TextView) findViewById(R.id.tv_wingman_status);
+        tvRoomStatus = (TextView) findViewById(R.id.tv_wingman_status);
+        tvLastSeen = (TextView) findViewById(R.id.tv_wingman_last_seen);
+        tvTimeElapsed = (TextView) findViewById(R.id.tv_time_elapsed);
 
         firebase.child("lastSeen").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 longDateValue[0] = (Long) dataSnapshot.getValue();
-                updateDateText(longDateValue[0], timeStampView);
+                updateDateText(longDateValue[0], tvRoomStatus, tvLastSeen, tvTimeElapsed);
             }
 
             @Override
@@ -61,7 +68,7 @@ public class MainActivity extends AppCompatActivity {
         refreshButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                updateDateText(longDateValue[0], timeStampView);
+                updateDateText(longDateValue[0], tvRoomStatus, tvLastSeen, tvTimeElapsed);
             }
         });
 
@@ -123,36 +130,47 @@ public class MainActivity extends AppCompatActivity {
                 }).show();
     }
 
-    public void updateDateText(long longDateValue, TextView timeStampView) {
+    public void updateDateText(long longDateValue, TextView roomStatus, TextView lastSeen, TextView timeGone) {
         SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yy");
         Date date = new Date(longDateValue);
         String strDate = formatter.format(date);
         formatter = new SimpleDateFormat("hh:mm:ss");
         String strTime = formatter.format(date);
         String dateText = "Movement last sensed at " + strTime + " on " + strDate;
+
         boolean roomIsEmpty = System.currentTimeMillis() - longDateValue > 60000;
-        if (roomIsEmpty) {
-            dateText +="\nRoom is probably empty\nMovement last sensed more than 1 minute ago";
-        } else {
-            dateText +="\nRoom is probably not empty\nMovement last sensed less than 1 minute ago";
-        }
+
+        String status = roomIsEmpty ? "Probably Empty" : "Probably Occupied";
+        int color = roomIsEmpty ? Color.parseColor("#009688") : Color.parseColor("#F44336");
+        String last = (roomIsEmpty) ? "More than 1 minute ago" : "Less than a minute ago";
+        String timeElapsed = "";
+
         if (System.currentTimeMillis() - longDateValue > 86400000) {
-            dateText += "\nYour room has been empty for more than one day.\nAre you sure your roommate isn't dead?";
+            last = "\nYour room has been empty for more than a day.\nAre you sure your roommate isn't dead?";
         } else {
             System.out.println(System.currentTimeMillis() - longDateValue);
             long systemDateValue = System.currentTimeMillis();
             Date differenceDate = new Date(systemDateValue - longDateValue + 2000);
             Date systemDate = new Date(systemDateValue);
             formatter = new SimpleDateFormat("hh");
-            dateText += "\nMovement last sensed " + (systemDateValue - longDateValue)/3600000;
-            //            dateText += "\nMovement last sensed " + formatter.format(new Date(systemDate.getHours()*3600000 - date.getHours()*3600000));
-            formatter = new SimpleDateFormat("mm");
-            dateText += " hours, " + differenceDate.getMinutes();
-            formatter = new SimpleDateFormat("ss");
-            dateText += " minutes and " +differenceDate.getSeconds();
-            dateText += " seconds ago";
-        }
-        timeStampView.setText(dateText);
-    }
 
+            long hour = (systemDateValue - longDateValue) / 3600000;
+            long minute = differenceDate.getMinutes();
+            long second = differenceDate.getSeconds();
+
+            timeElapsed += "Time Elapsed: " + hour + ((hour > 1) ? " hours, " : " hour, ")
+                    + minute + ((minute > 1) ? " minutes and " : " minute and ")
+                    + second + ((second > 1) ? " seconds" : " second");
+
+            //            dateText += "\nMovement last sensed " + formatter.format(new Date(systemDate.getHours()*3600000 - date.getHours()*3600000));
+//            formatter = new SimpleDateFormat("mm");
+//            formatter = new SimpleDateFormat("ss");
+        }
+
+        roomStatus.setText(status);
+        roomStatus.setTextColor(color);
+        lastSeen.setText(last);
+        timeGone.setText(timeElapsed);
+
+    }
 }
